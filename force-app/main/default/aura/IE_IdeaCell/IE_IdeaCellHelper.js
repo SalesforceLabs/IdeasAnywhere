ac9dec638ed3c6248d6d2af8cc9a7e4ed6fecb15
J-Saotome <jsaotome@salesforce.com>
2023-03-23 09:51:01 +0900

    Update License

--- force-app/main/default/aura/IE_IdeaCell/IE_IdeaCellHelper.js
+++ force-app/main/default/aura/IE_IdeaCell/IE_IdeaCellHelper.js
@@ -1,7 +1,7 @@
-// Copyright 2020 salesforce.com, inc
-// All rights reserved.
-// SPDX-License-Identifier: BSD-3-Clause
-// For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+//  Copyright (c) 2023, salesforce.com, inc.
+//  All rights reserved.
+//  SPDX-License-Identifier: BSD-3-Clause
+//  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 
 ({
 	pressLikeDislike: function(cmp, recordId, isLike, point) {
